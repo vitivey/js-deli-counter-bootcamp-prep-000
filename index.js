a11730edf9055d5e72203c2ae35fb56581afcb64
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-10 14:04:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function nowServing (katzDeli){
 function currentLine (katzDeli){
   let i=0;
   while (i < katzDeli.length){
+    lineInfo= [`${i+1}. ${katzDeli} `]
   i++;}
   
   if (katzDeli===0){return "The line is currently empty"}
