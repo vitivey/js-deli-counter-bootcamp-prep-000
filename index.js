1e648f0c8ca51f9e3a198cbc837a08018776ec17
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-10 13:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(katzDeli, name){
  return `Welcome, ${name}. You are number ${position} in line.`}
   
  
+<<<<<<< HEAD
 
 function nowServing (katzDeli){
   let i=0;
@@ -23,4 +24,30 @@ function currentLine (katzDeli, name){
   i++;}
   if (katzDeli===0){return "The line is currently empty"}
   else {`The line is currently: ${katzDeli.length[name]}. ${name}`}
+=======
+function nowServing (katzDeli){
+  let i=0;
+  while (i < katzDeli.length){
+  if (katzDeli.length ===  0) {
+    return "There is nobody waiting to be served!"}
+  else {return `Currently serving ${katzDeli.shift()}.`}
+  i++;
+}
+}
+
+function nowServing(katzDeli) {
++  let i = 0;
++  while (i < katzDeli.length) {
++    i++;
++  }
++  if (katzDeli.length === 0) {
++    
+
+
+
+
+function currentLine (){
+  if (katzDeli){return `The line is currently: ${katzDeli}. ${name}`}
+  else {"The line is currently empty"}
+>>>>>>> a7892b3d5a44543757ebd0fe9e16be068b6411b7
 }
